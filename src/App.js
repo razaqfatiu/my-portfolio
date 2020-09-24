bddde2584a071b9d7a7974c6ef9649e133270744
Fatiu Isiaka <fatiuisiaka@MacBook-Pro.local>
2020-09-24 18:31:09 +0100

    add netlify config

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import Contact from './Components/Pages/Contact';
+import Loading from './Components/Pages/Loading';
+import NavBar from './Components/Pages/NavBar';
+import Projects from './Components/Pages/Projects';
+import Blog from './Components/Pages/Blog';
+
+function App() {
+  return (
+    (<Loading /> &&
+      <Router>
+        <NavBar />
+
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+            <Projects />
+            <Contact />
+          </Route>
+
+          {/* <Route path='/projects'>
+            <Projects />
+          </Route>
+
+          <Route path='/contact'>
+            <Contact />
+          </Route> */}
+
+          <Route path='/blog'>
+            <Blog />
+          </Route>
+
+        </Switch>
+
+      </Router>
+    ));
+}
+
+export default App;
